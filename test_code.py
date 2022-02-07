0bd26bf4e538bee1367ee67f8d464f273fef6670
Fizinftop99 <naumov_nik99@mail.ru>
2022-02-07 12:19:57 +0300

    clear_database

--- test_code.py
+++ test_code.py
@@ -1,48 +1,22 @@
-from neo4j import GraphDatabase, Transaction
+from py2neo import Graph
 import pandas as pd
-
-
-def read_graph_data(file_name: str) -> pd.DataFrame:
-    df = pd.read_excel(file_name)
-    graph_data = df[['Идентификатор операции', 'Название операции', 'Предшественники', 'Последователи']]
-    graph_data['Идентификатор операции'] = graph_data['Идентификатор операции'].apply(str.strip)
-    graph_data = graph_data[graph_data['Идентификатор операции'].str.startswith('R')]
-    graph_data.set_index('Идентификатор операции', inplace=True)  # Update indeces
-    return graph_data
-
-
-def make_graph(tx: Transaction, data: pd.DataFrame):
-    id_lst = data.index.tolist()
-    for wrk_id in id_lst:
-        tx.run("MERGE (a:Work {id: $id, name: $name})", id=wrk_id,
-               name=data.loc[wrk_id, 'Название операции'])
-        # name=wrk_id)
-
-        s = data.loc[wrk_id, 'Последователи']
-        if s == s:  # Проверка на то, что есть Последователи (s != NaN)
-            followers = s.split(', ')
-            for flw_id in followers:
-                # if flw_id == "R1010":
-                #     print(wrk_id, "->", flw_id)
-                tx.run("MATCH (a:Work) WHERE a.id = $wrk_id "
-                       "MERGE (follower:Work {id: $flw_id, name: $flw_name}) "
-                       "MERGE (a)-[:FOLLOWS]->(follower)",
-                       wrk_id=wrk_id, flw_id=flw_id,
-                       flw_name=data.loc[flw_id, 'Название операции'])  # if flw_id in id_lst else flw_id)
+from numpy.random import randint
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+from sklearn.metrics import roc_curve, auc
+from collections import Counter
+from cycler import cycler
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 
 def main():
-    data = read_graph_data("2021-11-19 Roder связи.xlsx")
-    # data = read_graph_data("R1080.xlsx")
-    driver = GraphDatabase.driver("neo4j+s://174cd36c.databases.neo4j.io:7687",
-                                  auth=("neo4j", "w21V4bw-6kTp9hceHMbnlt5L9X1M4upuuq2nD7tD_xU"))
-    with driver.session() as session:
-        session.write_transaction(make_graph, data)
-        # session.write_transaction(add_friend, "Arthur", "Guinevere")
-        # session.write_transaction(add_friend, "Arthur", "Lancelot")
-        # session.write_transaction(add_friend, "Arthur", "Merlin")
-        # session.read_transaction(print_friends, "Arthur")
-    driver.close()
+    graph = Graph("bolt://localhost:7687", auth=("neo4j", "2310"))
 
 
 if __name__ == "__main__":
